17cdd08a2a42ea3b19b473c73096d78dc7611633
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-03-05 13:47:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-this.Image
+function NewImage (url, left,bottom){
+    let object = document.createElement(`img`)
+    object.src= url
+    object.style.left= left+`px`
+    object.style.bottom = bottom + `px`
+    object.style.position= `fixed`
+    document.body.append(object)
+    return object
+    
+}
+function newEnimy (url,left,bottom){
+    let object=NewImage(url,left,bottom)
+    
+}
+
+
+newEnimy(`./enemy1.png`,550,550)
+NewImage(`player.png`,50,50)
